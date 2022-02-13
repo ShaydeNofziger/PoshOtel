fc2161dff79b8f40dea03a3e1b2f49dfcaf8be91
Shayde Nofziger <shayde@hey.com>
2022-02-13 15:37:25 -0500

    Store packages.config alongside src code

--- tools/install.ps1
+++ tools/install.ps1
@@ -1,4 +1,8 @@
+$SrcDir = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath 'src'
+$BinDir = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath 'bin'
+
 # Get nuget.exe
 if (-not (Test-Path -Path $PSScriptRoot\nuget.exe)) { Invoke-WebRequest -Uri 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile "$PSScriptRoot\nuget.exe" }
-. $PSScriptRoot\nuget.exe install $PSScriptRoot\packages.config -OutputDirectory $PSScriptRoot\..\bin\packages -ExcludeVersion
 
+Write-Verbose -Message 'Installing packages...'
+. $PSScriptRoot\nuget.exe install $SrcDir\packages.config -OutputDirectory $BinDir\packages -ExcludeVersion
